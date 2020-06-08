eb679217ce38b4e0a592e4d736cb3e69af6387fd
Chunhao Zheng <chz062009@gmail.com>
2020-06-08 08:24:56 -0400

    Updates

---
+++ precache-manifest.fbb50d9a591647ac317869773b2da312.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abf3d50c7c7f6ac2842e21e1a256a38",
+    "url": "/assignment-8-dom-react/index.html"
+  },
+  {
+    "revision": "f89d4f8632300bd5c6ca",
+    "url": "/assignment-8-dom-react/static/css/main.23edde5c.chunk.css"
+  },
+  {
+    "revision": "0b73e3a1ddf8f7115ffe",
+    "url": "/assignment-8-dom-react/static/js/2.4a36d7aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assignment-8-dom-react/static/js/2.4a36d7aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f89d4f8632300bd5c6ca",
+    "url": "/assignment-8-dom-react/static/js/main.4b39052a.chunk.js"
+  },
+  {
+    "revision": "191f90e48dadda337821",
+    "url": "/assignment-8-dom-react/static/js/runtime-main.a2b421c5.js"
+  }
+]);
